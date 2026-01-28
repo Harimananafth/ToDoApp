e344af7ca4429c8ee8cd9a5200d5a355466202e8
harimanana.fth <harimananafth@gmail.com>
2026-01-28 22:40:35 +0300

    First commt

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import Clipboard from "./components/clipboard";
+export default function App() {
+  return (
+    <div className="min-h-screen bg-base-100 flex items-center justify-center pt-10">
+      <Clipboard />
+    </div>
+  );
+}
---
+++ client/src/components/clipboard.jsx
@@ -0,0 +1,7 @@
+export default function Clipboard(){
+    return (
+        <div className="h-full w-2xl bg-">
+            
+        </div>
+    )
+}
